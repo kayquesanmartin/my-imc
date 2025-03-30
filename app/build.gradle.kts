d82cbfb92d7d46782ddbfff606d0a29996224e33
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-03-30 12:40:53 -0300

    chore: add UI elements for height and weight input

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.kayquesanmartin.myimc"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
