39a0ee05c942e8d4014cf686a14c4f4204387649
kayquesanmartin <kayque.sanmartin65@gmail.com>
2025-03-31 10:55:03 -0300

    feat: improved layout, added functionality for BMI calculation and validation of input fields

--- app/src/main/java/io/github/kayquesanmartin/myimc/MainActivity.kt
+++ app/src/main/java/io/github/kayquesanmartin/myimc/MainActivity.kt
@@ -1,27 +1,79 @@
 package io.github.kayquesanmartin.myimc
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
 import android.widget.EditText
-import android.widget.Spinner
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var resultTextView: TextView
+    private lateinit var mainButton: Button
     private lateinit var heightEditText: EditText
-    private lateinit var spinnerHeightType: Spinner
+    private lateinit var weightEditText: EditText
+
+    fun validateInput(): Boolean {
+
+        val height = heightEditText.text.toString()
+        val weight = weightEditText.text.toString()
+
+        if (TextUtils.isEmpty(height)) {
+            heightEditText.error = "Altura é obrigatória"
+            heightEditText.requestFocus()
+            return false
+        }
+
+        if (height.toDoubleOrNull() == null) {
+            heightEditText.error = "Altura inválida"
+            heightEditText.requestFocus()
+            return false
+        }
+
+        if (TextUtils.isEmpty(weight)) {
+            weightEditText.error = "Peso é obrigatório"
+            weightEditText.requestFocus()
+            return false
+        }
+
+        if (weight.toDoubleOrNull() == null) {
+            weightEditText.error = "Peso inválido"
+            weightEditText.requestFocus()
+            return false
+        }
+
+        return true
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        resultTextView = findViewById(R.id.resultTextView)
+        mainButton = findViewById(R.id.mainButton)
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+
+        mainButton.setOnClickListener {
+            if (validateInput()) {
+                val height: Double = heightEditText.text.toString().toDouble()
+                val weight: Double = weightEditText.text.toString().toDouble()
+
+                val imc = weight / (height * height)
+
+                when {
+                    imc < 18.5 -> resultTextView.text = getString(R.string.underweight)
+                    imc in 18.5..24.9 -> resultTextView.text = getString(R.string.normal)
+                    imc in 25.0..29.9 -> resultTextView.text = getString(R.string.overweight)
+                    imc in 30.0..34.9 -> resultTextView.text = getString(R.string.obesityI)
+                    imc in 35.0..39.9 -> resultTextView.text = getString(R.string.obesityII)
+                    else -> resultTextView.text = getString(R.string.obesityIII)
+                }
+            }
         }
     }
 }
